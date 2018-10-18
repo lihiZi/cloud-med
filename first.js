75a72d7e734030cdfa0b6b2691bfabb289789d25
Lihi ZIGER <lihi@Lihis-MacBook-Air.local>
2018-10-18 12:39:42 -0400

    initial commit

---
+++ first.js
@@ -0,0 +1,59 @@
+
+
+
+
+window.setInterval( changeProduct, 5000);
+var i = 1 ; 
+console.log(i);
+
+function changeProduct () {
+
+}
+
+
+
+function changeProduct () {
+
+	if (i ==1) {
+		document.getElementById("product-img").className="product-img-1";
+		document.getElementById("product-name").innerHTML = "Nittaya SW"
+		document.getElementById("product-spec").innerHTML = " A SaaS desktop and mobile application <br> that process and connect patients with pediatric services";
+		i++;
+		console.log(i);
+	} else if (i==2) {
+		document.getElementById("product-img").className="product-img-2";
+		document.getElementById("product-name").innerHTML = "Stetho-USB";
+		document.getElementById("product-spec").innerHTML = " An IoT stethoscope that records digital measurements dirclt to Nitatya SW"
+		i++;	
+	} else if (i==3){
+		document.getElementById("product-img").className="product-img-3";
+		document.getElementById("product-name").innerHTML = "Throught- Camera"
+		document.getElementById("product-spec").innerHTML = " Smart Camera that process images of inner throught and records on Nittaya"
+		i++;
+	} else if (i==4) {
+		document.getElementById("product-img").className="product-img-4";
+		document.getElementById("product-name").innerHTML = "Thermo-USB"
+		document.getElementById("product-spec").innerHTML = "An easy to use thermometer that recirds measurements directly on Nittaya" 
+		 i=1;
+	}
+	
+/*	if (i ==2) {
+			document.getElementById("product-img").className="product-img-2";
+			i++;
+			console.log(i);
+
+		}
+	if (i ==3) {
+		document.getElementById("product-img").className="product-img-3";
+		i++;
+		console.log(i);
+	}
+	if (i ==4) {
+		document.getElementById("product-img").className="product-img-4";
+		i=1;
+		console.log(i);
+	}
+*/
+
+
+}
